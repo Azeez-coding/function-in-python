bbd00a43f8867f90921e6215c25b5304ddea03d1
Azeez-coding <meetmusaonline03@gmail.com>
2024-12-21 17:45:55 +0100

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+num1=int(input("enter no 1"))
+num2=int(input("enter no 2"))
+
+print("the additon of two no is",add(num1,num2))
+print("the difference of two no is",sub(num1,num2))
+print("the product of two no is",mul(num1,num2))
---
+++ main.py
@@ -0,0 +1,10 @@
+print ("hello")
+# def display();
+# print ("hello i am azeez")
+# display ()
+
+def display (name,age):
+    print("hello",name," hi i am",age,"years old")
+name=input("what is your name")
+age=int(input("what is your age"))
+display(name,age)
